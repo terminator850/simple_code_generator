31f7953c92c46975e1073f5c830a4243a3e619de
wangyu <wangyu2017@qq.com>
2019-11-18 23:21:30 +0800

    repaired

--- main.go
+++ main.go
@@ -3,34 +3,43 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"os"
+	"text/template"
 )
 
-type Protocol struct {
-	tid_def Tiddef `xml:"tid_def"`
+type protocol struct {
+	Tid_def tiddef `xml:"tid_def"`
 }
 
-type Tiddef struct {
-	tid []Tid `xml:"tid"`
+type tiddef struct {
+	Tid []tid `xml:"tid"`
 }
 
-type Tid struct {
-	name  string `xml:"name,attr"`
-	value string `xml:"value,attr"`
-	dese  string `xml:"desc,attr"`
+type tid struct {
+	Name  string `xml:"name,attr"`
+	Value string `xml:"value,attr"`
+	Dese  string `xml:"desc,attr"`
 }
 
 func main() {
 	xml_data := `<?xml version="1.0" encoding="utf-8"?>
 	<protocol>
 		<tid_def>
-			<tid name="TID_REQ_LOGIN" value="10000" desc = "交易远登陆请求">
-			<tid name="TID_RSP_LOGIN" value="10001" desc = "交易远登陆请求">
+			<tid name="TID_REQ_LOGIN" value="10000" desc = "交易远登陆请求"/>
+			<tid name="TID_RSP_LOGIN" value="10001" desc = "交易远登陆应答"/>
 		</tid_def>
 	</protocol>`
 
-	protocol := new(Protocol)
+	Protocol := new(protocol)
 
-	xml.Unmarshal([]byte(xml_data), protocol)
-	fmt.Println(*protocol)
+	err1 := xml.Unmarshal([]byte(xml_data), Protocol)
+	fmt.Println(err1)
 
+	fmt.Println(*Protocol)
+
+	t := template.New("temp")
+	t, _ = t.Parse(`{{range .Tid_def.Tid}}
+{{.Name}}
+{{end}}`)
+	t.Execute(os.Stdout, Protocol)
 }
